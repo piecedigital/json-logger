07fded6798cf5706dd41577ddec22d724d1faf2a
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-10-20 23:03:17 -0400

    added line counter. included multiply prototype method in function

--- json-logger.js
+++ json-logger.js
@@ -1,36 +1,36 @@
-String.prototype.multiply = function(times) {
-	var arr = [];
-	var tick = 0;
-	while(tick < times) {
-		arr.push(this);
-		tick++;
-	}
-
-	return arr.join("");
-};
-
 // an object with nested data
 var object = {
 	text1: "text1",
 	object1: {
-		obj1: {},
-		obj2: {},
-		obj3: {}
+		obj1: {
+		},
+		obj2: {
+		},
+		obj3: {
+		}
 	},
 	text2: "text2",
 	text3: "text3",
 	object3: {
-		obj1: {},
+		obj1: {
+		},
 		obj2: {
-			obj1: {},
-			obj2: {},
+			obj1: {
+			},
+			obj2: {
+			},
 			obj3: {
-				obj1: {},
-				obj2: {},
+				obj1: {
+				},
+				obj2: {
+				},
 				obj3: {
-					obj1: {},
-					obj2: {},
-					obj3: {}
+					obj1: {
+					},
+					obj2: {
+					},
+					obj3: {
+					}
 				}
 			}
 		},
@@ -38,46 +38,74 @@ var object = {
 	},
 	text4: "text4",
 	object4: {
-		obj1: {},
-		obj2: {},
-		obj3: {}
+		obj1: {
+		},
+		obj2: {
+		},
+		obj3: {
+		}
 	},
 	text5: "text5",
 	text6: "text6",
 	object6: {
-		obj1: {},
-		obj2: {},
-		obj3: {}
+		obj1: {
+		},
+		obj2: {
+		},
+		obj3: {
+		}
 	},
 	text7: "text7",
 	object7: {
-		obj1: {},
-		obj2: {},
-		obj3: {}
+		obj1: {
+		},
+		obj2: {
+		},
+		obj3: {
+		}
 	}
 };
 
 // this function will loop through the object recursively and
-var jsonLogger = function(obj, level) {
-	var data = obj, level = level || 1;
+var jsonLogger = function(obj, level, line) {
+	// adds multiply prototype method, to assure it exist
+	if(!("pd_multiply" in String.prototype)) {
+		console.log("added pd_multiply");
+		String.prototype.pd_multiply = function(times) {
+			var arr = [];
+			var tick = 0;
+			while(tick < times) {
+				arr.push(this);
+				tick++;
+			}
+
+			return arr.join("");
+		};
+	}
+
+	var data = obj, level = level || 1, line =  line || 1;
 
 	for(var key in data) {
 		// if the type of value on the current key is equal to "object"
 		if(typeof data[key] === "object") {
 			// ... log braces and they key
-			console.log(level, " ".multiply(level-1) + key + " {");
+			console.log(line +":", " ".pd_multiply(level-1) + key + " {");
+			line++;
 			// ... then call jsonLogger, passing the object in the current key,
 			// ... and passing the next level, for formatting
-			jsonLogger(data[key], level+1);
-			console.log(level, " ".multiply(level-1) + "}");
+			line = jsonLogger(data[key], level+1, line);
+			console.log(line +":", " ".pd_multiply(level-1) + "}");
+			line++;
 		} else {
 			// ... if not an object, simply log the key and value
-			console.log(level, key + ": " + data[key])
+			console.log(line, key + ": " + data[key]);
+			line++;
 		}
 	}
+	return line;
 }
 
-jsonLogger(object, 1);
+jsonLogger(object);
 
 // created by me :)
 var creator ={};
